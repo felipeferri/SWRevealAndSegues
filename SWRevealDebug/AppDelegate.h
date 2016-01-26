a418f677f53dfd0aca623de7c59292f31498b66b
Felipe Augusto Sviaghin Ferri <felipe.ferri@gmail.com>
2016-01-26 16:37:27 -0200

    Initial Commit

---
+++ SWRevealDebug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWRevealDebug
+//
+//  Created by Felipe Ferri on 26/01/16.
+//  Copyright © 2016 Felipe Ferri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
